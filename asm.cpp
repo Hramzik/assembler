93d7aa7a1a694863362323b7fd3ea36d168ace7d
Vladz <hramzik.ru@yandex.ru>
2022-10-08 18:12:09 +0300

    back to all-in-one

---
+++ asm.cpp
@@ -0,0 +1,195 @@
+
+
+#include "asm.hpp"
+
+#include "lib/onegin.hpp"
+#include "lib/stack.hpp"
+
+
+Command_code  get_command_name  (char* command) {
+
+    if (command == nullptr) { return UNKNOWN; }
+
+
+    if ( !stricmp (command, "unknown"))                                 { return UNKNOWN; }
+    if ( !stricmp (command, "halt")      || !stricmp (command, "hlt"))  { return HALT; }
+    if ( !stricmp (command, "out"))                                     { return OUT; }
+    if ( !stricmp (command, "push"))                                    { return PUSH; }
+    if ( !stricmp (command, "pop"))                                     { return POP; }
+    if ( !stricmp (command, "add"))                                     { return ADD; }
+    if ( !stricmp (command, "substract") || !stricmp (command, "sub"))  { return SUBSTRACT; }
+    if ( !stricmp (command, "multiply")  || !stricmp (command, "mult")) { return MULTIPLY; }
+    if ( !stricmp (command, "divide")    || !stricmp (command, "div"))  { return DIVIDE; }
+
+    return UNKNOWN;
+}
+
+
+Return_code assembler  (const char* source_name, const char* out_name) {
+
+    if (source_name == nullptr || out_name == nullptr) { return BAD_ARGS; }
+
+
+    FILE* source = fopen (source_name, "r");
+    FILE* out    = fopen (   out_name, "wb");
+
+
+    if (source == nullptr || out == nullptr) { return FILE_ERR; }
+
+
+
+    Preamble preamble = {'W', 'W', 1.0};
+    fwrite ( &preamble, Preamble_size, 1, out);/*
+    fwrite ( &preamble.signature_first_letter, sizeof preamble.signature, 1, out);
+    fwrite ( &preamble.version,   sizeof preamble.version,   1, out);*/
+
+
+    Text* source_lines = initialize_text (source_name); //КОСТЫЛЬ!!! ПЕРЕПИСАТЬ!!!
+
+
+    char   command [MAX_COMMAND_LEN] = "";
+    size_t commands_size             = source_lines->num_lines * (Command_size + Argument_size);
+    void*  commands                  = calloc (1, commands_size);
+
+    for (size_t line_ind = 0, bytes_filled = 0, command_ind = 0; line_ind < source_lines->num_lines; line_ind++) {
+
+
+        sscanf (source_lines->lines[line_ind].ptr, "%s", command); printf ("%s ", command);
+
+
+        Command_code command_code = get_command_name (command); printf ("%d\n", command_code);
+        Command_mode command_mode = 0;
+        Argument     argument;
+
+
+        switch (command_code) {
+
+            case UNKNOWN:
+
+                LOG_ERROR (BAD_ARGS);
+                return BAD_ARGS;
+
+            case HALT:
+
+                binary_array_write       (commands, &command_code, Command_size, &bytes_filled);
+
+
+                IF_CREATING_LISTING_FILE (listing_write (command_ind, command_code, command));
+                break;
+
+            case OUT:
+
+                binary_array_write       (commands, &command_code, Command_size, &bytes_filled);
+
+
+                IF_CREATING_LISTING_FILE (listing_write (command_ind, command_code, command));
+                break;
+
+            case PUSH:
+
+                sscanf (source_lines->lines[line_ind].ptr, "%*s %lf", &argument);
+
+                command_mode = 0;                     //KOSTIL!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                binary_array_write (commands, &command_code, Command_size,      &bytes_filled);
+                binary_array_write (commands, &command_mode, Command_mode_size, &bytes_filled);
+                binary_array_write (commands, &argument,     Argument_size,     &bytes_filled);
+
+
+                IF_CREATING_LISTING_FILE (listing_write (command_ind, command_code, command, argument, command_mode));
+                break;
+
+            case POP:
+
+                binary_array_write       (commands, &command_code, Command_size, &bytes_filled);
+
+
+                IF_CREATING_LISTING_FILE (listing_write (command_ind, command_code, command));
+                break;
+
+            case ADD:
+
+                binary_array_write       (commands, &command_code, Command_size, &bytes_filled);
+
+
+                IF_CREATING_LISTING_FILE (listing_write (command_ind, command_code, command));
+                break;
+
+            case SUBSTRACT:
+
+                binary_array_write       (commands, &command_code, Command_size, &bytes_filled);
+
+
+                IF_CREATING_LISTING_FILE (listing_write (command_ind, command_code, command));
+                break;
+
+            case MULTIPLY:
+
+                binary_array_write       (commands, &command_code, Command_size, &bytes_filled);
+
+
+                IF_CREATING_LISTING_FILE (listing_write (command_ind, command_code, command));
+                break;
+
+            case DIVIDE:
+
+                binary_array_write       (commands, &command_code, Command_size, &bytes_filled);
+
+
+                IF_CREATING_LISTING_FILE (listing_write (command_ind, command_code, command));
+                break;
+
+            default:
+
+                LOG_ERROR (BAD_ARGS);
+                return BAD_ARGS;
+        }
+
+        command_ind++;
+    }
+
+    fwrite ( commands, commands_size, 1, out);
+
+
+    cleanmemory (source_lines);
+
+
+    return SUCCESS;
+}
+
+
+Return_code  binary_array_write  (void* array, void* filler, size_t size, size_t* bytes_filled) {
+
+    memcpy ( (char*) array + *bytes_filled, filler, size);
+    *bytes_filled += Command_size;
+
+
+    return SUCCESS;
+}
+
+
+Return_code  listing_write  (size_t command_ind, Command_code command_code, char* command, Argument argument, Command_mode command_mode) {
+
+    FILE* listing_file = fopen (default_listing_file_name, "a"); //static
+    //static bool  first_function_call_flag = true;
+
+
+    fprintf                           (listing_file, "%04zd %04X", command_ind, command_code);
+
+    if (command_mode != -1) { fprintf (listing_file, " %04X",      command_mode); }
+    else                    { fprintf (listing_file, "     "); }
+
+    if ( !isnan (argument)) { fprintf (listing_file, " %2.1lf",       argument); }
+    else                    { fprintf (listing_file, "     "); }
+    fprintf                           (listing_file, " %s",        command);
+    if ( !isnan (argument)) { fprintf (listing_file, " %2.1lf",     argument); }
+    fprintf                           (listing_file, "\n");
+
+
+    fclose (listing_file); //КОСТЫЛЬ!!!!! ПЕРЕДЕЛАТЬ!!!!!!!!!!
+    //if (first_function_call_flag) { atexit (fclose (listing_file)); }
+
+
+    return SUCCESS;
+}
+
+
---
+++ asm.hpp
@@ -0,0 +1,74 @@
+
+
+#include <sys\stat.h>
+#include <locale.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+
+
+//-------------------- SETTINGS --------------------
+#define CREATE_LISTING_FILE
+
+//#define work_folder               work/
+#define default_listing_file_name "work/listing.txt"
+#define default_source_file_name  "work/code.txt"
+#define default_output_file_name  "work/out.txt"
+
+const size_t MAX_COMMAND_LEN = 40;
+//--------------------------------------------------
+
+
+#ifdef CREATE_LISTING_FILE
+    #define IF_CREATING_LISTING_FILE(x) x
+#else
+    #define IF_CREATING_LISTING_FILE(x)
+#endif
+
+enum  Return_code  {
+
+    SUCCESS    = 0,
+    MEMORY_ERR = 1,
+    BAD_ARGS   = 2,
+    FILE_ERR   = 3,
+};
+
+
+enum Command_code {
+
+    UNKNOWN   = 0,
+    HALT      = 1,
+    OUT       = 2,
+    PUSH      = 3,
+    POP       = 4,
+    ADD       = 5,
+    SUBSTRACT = 6,
+    MULTIPLY  = 7,
+    DIVIDE    = 8,
+};
+
+
+struct Preamble_struct {
+
+    char   signature_first_letter;
+    char   signature_second_letter;
+    double version;
+};
+
+
+typedef Preamble_struct Preamble;
+
+typedef double Argument;
+typedef char   Command_mode;
+
+
+const size_t Command_size      = sizeof (Command_code);
+const size_t Command_mode_size = sizeof (Command_mode);
+const size_t Argument_size     = sizeof (Argument);
+const size_t Preamble_size     = sizeof (Preamble);
+
+
+Command_code get_command_name   (char* command);
+Return_code  assembler          (const char* source_name = default_source_file_name, const char* out_name = default_output_file_name);
+Return_code  binary_array_write (void* array, void* filler, size_t size, size_t* bytes_filled);
+Return_code  listing_write      (size_t command_ind, Command_code command_code, char* command, Argument argument = NAN, Command_mode command_mode = -1);
